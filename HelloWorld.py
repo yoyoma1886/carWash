dc57a4af63919afa470d88f16742fd6552c40880
unknown <yoyoma1885@gmail.com>
2019-11-13 19:51:35 -0500

    55min mark change

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,19 +1,18 @@
-import math
-from os import rename
-import sys
+# import math
+# from os import rename
+# import sys
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = 'Hello, {}'.format(who_to_greet)
+#     return greeting
 
 
 r = requests.get('https://coreyms.com')
 print(r.status_code)
-
-# 51 minutes, trying to install git - email
+print(r.ok)
